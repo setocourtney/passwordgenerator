7677f96c0f3c4ffbe978fac241270f29d9df4bd5
Courtney Seto <courtneyjseto@gmail.com>
2019-12-16 20:26:19 -0800

    added index and script with pseudo code

---
+++ script.js
@@ -0,0 +1,16 @@
+// button initiates prompt
+
+//prompt how many characters between 8 and 28
+
+//prompt what kind of characters (special, numeric, lowercase, uppercase)
+
+//validate user input = min 1 of allowable types
+
+//array of characters = length of prompted characters
+//array of user selected possible values
+//array of possible values for each type
+//boolean for each character type
+//if true for character type, add possible values to user selected array
+
+//loop through array of characters
+//add character from random index
